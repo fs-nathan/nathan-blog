e87f6a31e2f2b72d198896199f3b9865493099b8
fs-nathan <nathan.makeitpro@gmail.com>
2022-03-18 17:37:12 +0700

    Fix typo in site name

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,3 +1,3 @@
 export const SITE_URL = "http://localhost:3000";
-export const SITE_NAME = "Nathan' Coding Blog";
+export const SITE_NAME = "Nathan's Coding Blog";
 export const TWITTER_USERNAME = "@nathanmakeit";
