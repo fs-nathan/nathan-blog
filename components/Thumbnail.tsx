e936b35cd77a40722739991cb1ab545ee916c36b
fs-nathan <nathan.codeaholic@gmail.com>
2022-03-19 17:34:02 +0700

    Thumbnail: resizing

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -9,12 +9,8 @@ type Props = {
 
 const Thumbnail: React.FC<Props> = ({ title, src, slug }: Props) => {
     const image = (
-        <Image
-            src={src}
-            alt={`${title}`}
-            width={1280}
-            height={720}
-        />
+        <Image src={src} alt={title} width="100%" height="100%" layout="responsive" objectFit="contain"/>
+
     );
     return (
         <>
